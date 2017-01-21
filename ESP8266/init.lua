602fea323c425cf7fcbfd13931dd12c7307f0095
michaelmertens <mertens.michael@hotmail.com>
2017-01-21 17:37:27 +0100

    new index.html

--- ESP8266/init.lua
+++ ESP8266/init.lua
@@ -9,6 +9,7 @@ srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
         local buf = "";
+        buf = buf.."HTTP/1.1 200 OK\n\n"
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
         if(method == nil)then
             _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
@@ -19,10 +20,26 @@ srv:listen(80,function(conn)
                 _GET[k] = v
             end
         end
-        buf = buf.."<h1> ESP8266 Web Server</h1>";
-        buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
-        buf = buf.."<p>GPIO2 <a href=\"?pin=ON2\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>OFF</button></a></p>";
-        local _on,_off = "",""
+        buf = buf.."<head>";
+        buf = buf.."<meta charset=\"utf-8\">";
+        buf = buf.."<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">";
+        buf = buf.."<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+        buf = buf.."<script src=\"https://code.jquery.com/jquery-2.1.3.min.js\"></script>";
+        buf = buf.."<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">";
+        buf = buf.."</head><div class=\"container\">";
+
+        buf = buf.."<h1>Web Server</h1>";
+        buf = buf.."<h2>GPIO 0</h2>";
+        buf = buf.."<div class=\"row\">";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=ON1\" class=\"btn btn-block btn-lg btn-success\" role=\"button\">ON</a></div>";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=OFF1\" class=\"btn btn-block btn-lg btn-danger\" role=\"button\">OFF</a></div>";
+        buf = buf.."</div>";
+        buf = buf.."<h2>GPIO 2</h2>";
+        buf = buf.."<div class=\"row\">";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=ON2\" class=\"btn btn-block btn-lg btn-primary\" role=\"button\">ON</a></div>";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=OFF2\" class=\"btn btn-block btn-lg btn-warning\" role=\"button\">OFF</a></div>";
+        buf = buf.."</div></div>";
+        
         if(_GET.pin == "ON1")then
               gpio.write(led1, gpio.HIGH);
         elseif(_GET.pin == "OFF1")then
